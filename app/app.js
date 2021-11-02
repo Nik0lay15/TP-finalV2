e70d6c521643d595a6dc519407ebc3d7a1b044b0
Manuel <manueltello41@gmail.com>
2021-11-02 00:36:01 -0300

    Comentarios cambiados

--- app/app.js
+++ app/app.js
@@ -1,15 +1,12 @@
-//--> Dependencias
 import express  from "express";
 import router_carrito from "../routes/carrito.js";
 import router_productos from "../routes/productos.js";
 import router_error from "../routes/error_handler.js";
 const app = express();
 
-//--> Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//--> Rutas
 app.use("/carrito",router_carrito);
 app.use("/productos",router_productos);
 app.use(router_error);
